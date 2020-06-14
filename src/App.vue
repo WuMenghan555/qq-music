855cb0d3e0ad451855f99323c6bf2eb900ff98cd
wmh0524 <2668999690@qq.com>
2020-06-14 23:53:30 +0800

    header+tab页面开发

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <div id="app">hellow world</div>
+  <div id="app">
+    <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
+import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
+export default {
+  components: {
+    MHeader,
+    Tab
+  }
+};
 </script>
 
-<style scoped lang="stylus" rel="stylesheet/stylus">
-@import '~common/stylus/variable';
-  #app {
-    color: $color-theme
-  }
-</style>
+<style scoped lang="stylus" rel="stylesheet/stylus"></style>
